9ccbdac8734ba1391e55bd6bebc91eab483347e9
D'Amico Giuseppe <damicogiuseppe77@gmail.com>
2014-08-01 09:14:07 +0200

    first commit

---
+++ www/js/controllers.js
@@ -0,0 +1,76 @@
+angular.module('starter.controllers', ['ngCordova'])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, $cordovaBarcodeScanner) {
+  // Form data for the login modal
+  $scope.loginData = {};
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  },
+ $scope.scan = function(){
+     $cordovaBarcodeScanner.scan().then(function(result){
+         alert("I have Data");
+     },function(error){
+         alert("I have error");
+     })
+ }
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+})
+
+.controller('PlaylistsCtrl', function($scope) {
+  $scope.playlists = [
+    { title: 'Reggae', id: 1 },
+    { title: 'Chill', id: 2 },
+    { title: 'Dubstep', id: 3 },
+    { title: 'Indie', id: 4 },
+    { title: 'Rap', id: 5 },
+    { title: 'Cowbell', id: 6 }
+  ];
+})
+
+.controller('PlaylistCtrl', function($scope, $stateParams) {
+}).controller('BarcodeScannerCtrl', function($scope, $cordovaBarcodeScanner) {
+
+  $scope.scanBarcode = function() {
+    $cordovaBarcodeScanner.scan().then(function(imageData) {
+      // Success! Barcode data is here
+
+    }, function(err) {
+      // An error occured. Show a message to the user
+
+    });
+  };
+
+  // NOTE: encoding not functioning yet
+  $scope.encodeData = function() {
+    $cordovaBarcodeScanner.encode(BarcodeScanner.Encode.TEXT_TYPE, "http://www.nytimes.com").then(function(success) {
+      // Success! 
+    }, function(err) {
+      // An error occured. Show a message to the user
+
+    });      
+  }
+});
